a99556c2dc5fdb7d1fd8c1f8ec61d2ec55129f5f
thaithingocly-hv <thaithingocly.hv@gmail.com>
2026-03-01 21:56:17 +0700

    feat: add rating model and serializer

--- realEstate/settings/base.py
+++ realEstate/settings/base.py
@@ -125,3 +125,50 @@ MEDIA_ROOT = BASE_DIR / "mediafiles" # Ly-28.02.2026
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import logging
+import logging.config
+
+from django.utils.log import DEFAULT_LOGGING
+
+logger = logging.getLogger(__name__)
+
+LOG_LEVEL = "INFO" # Ly-27.02.2026
+
+logging.config.dictConfig({
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+       "console": {
+            "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+            
+        },
+       "file": {
+            "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+        },
+       "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "console",
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "formatter": "file",
+            "filename": "logs/real_estate.log",
+        },
+        "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
+    },
+    "loggers": {
+        "": {"handlers": ["console", "file"],"level": "INFO","propagate": False,
+        },
+        "apps": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
+    },
+})
